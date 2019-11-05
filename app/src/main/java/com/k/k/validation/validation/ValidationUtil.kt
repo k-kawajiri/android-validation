bf6771e1e629c3da72d9c84e0389236fd0d38351
k-kawajiri <kawanojieee@gmail.com>
2019-11-05 16:47:04 +0900

    [add]バリデーションエラーの存在を判定するメソッドを追加する

---
+++ app/src/main/java/com/k/k/validation/validation/ValidationUtil.kt
@@ -0,0 +1,29 @@
+package com.k.k.validation.validation
+
+
+object ValidationUtil {
+
+    /**
+     * バリデーションエラーが一つでもあるか判定する.
+     *
+     * @param validateLiveData {@link ValidateLiveData}の配列
+     * @return true:バリデーションエラーが一つでもある
+     *         false: バリデーションエラーがない
+     */
+    @JvmStatic
+    fun anyError(vararg validateLiveData: ValidateLiveData): Boolean {
+        return validateLiveData.any { it.isError }
+    }
+
+    /**
+     * バリデーションエラーがないことを判定する.
+     *
+     * @param validateLiveData {@link ValidateLiveData}の配列
+     * @return true:バリデーションエラーがない
+     *         false: バリデーションエラーが一つでもある
+     */
+    @JvmStatic
+    fun noneError(vararg validateLiveData: ValidateLiveData): Boolean {
+        return validateLiveData.none { it.isError }
+    }
+}
